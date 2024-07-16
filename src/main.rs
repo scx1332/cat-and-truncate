3ff7da5e77d95281cb1e77992cf93c84372eb20a
scx1332 <sieciech.czajka@golem.network>
2024-07-16 19:26:07 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use clap::Parser;
 use std::env;
 use std::fs::{File, OpenOptions};
-use std::io::{self, Write, Read};
-use std::path::Path;
-use clap::Parser;
+use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
+use std::thread::sleep;
 
 /// A simple program to read a file name and length from the command line
 #[derive(Parser, Debug)]
@@ -10,70 +11,90 @@ use clap::Parser;
 struct Args {
     /// Name of the file to read
     #[clap(short, long)]
-    file: Option<String>,
+    file: String,
 
-    /// Size of the chunk to read at once
-    #[clap(short, long, default_value = "1000000")]
-    chunk: usize,
+    /// Percent of file at which truncate should be performed
+    #[clap(short, long, default_value = "50")]
+    truncate: f64,
 }
 
-fn cat_file(file_path: &Path) -> anyhow::Result<()> {
+fn cat_file(file_path: &str, drop_percent: f64) -> anyhow::Result<()> {
     let mut buffer = Vec::new();
     let mut stdout = io::stdout();
     // Read the file
-
     'outer: loop {
+        let drop_bytes = (drop_percent / 100.0) * std::fs::metadata(file_path).unwrap().len() as f64;
+        let drop_bytes = drop_bytes as usize + 10000000;
+        log::info!("Reading file {}, drop at {}% - limit bytes {}", file_path, drop_percent, drop_bytes);
         //open file and check if you have write permission at the same time
-        let mut file = OpenOptions::new().read(true).write(true).truncate(false).open(file_path).unwrap();
-        let mut file_copy = File::create("random_1gb_file_copy.bin").unwrap();
+        let mut file = OpenOptions::new()
+            .read(true)
+            .truncate(false)
+            .open(&file_path)
+            .unwrap();
+        let file_path_copy = format!("{file_path}.part");
         let mut local_bytes_read = 0;
         loop {
-            let mut buffer = Vec::new();
             buffer.resize(1000 * 1000, 0);
-            let bytes_read = file.read(&mut buffer.as_mut_slice())?;
+            let bytes_read = file.read(&mut buffer.as_mut_slice()).unwrap();
             if bytes_read == 0 {
                 break;
+            }
+            if bytes_read < buffer.len() {
+                buffer.resize(bytes_read, 0);
             }
             local_bytes_read += bytes_read;
 
             stdout.write_all(&buffer)?;
 
             stdout.flush()?;
-            if local_bytes_read > 10000000 {
+            if local_bytes_read > drop_bytes {
                 break;
             }
         }
-        log::info!("Writing rest of the file to file_copy {}/{}", local_bytes_read, std::fs::metadata("random_1gb_file.bin")?.len());
+        let mut bytes_written = 0;
+        let mut file_copy = File::create(&file_path_copy).unwrap();
+        log::info!(
+            "Writing rest of the file to {}. {}/{}",
+            file_path_copy,
+            local_bytes_read,
+            std::fs::metadata(file_path).unwrap().len()
+        );
         loop {
-            let bytes_read = file.read(&mut buffer.as_mut_slice())?;
+            buffer.resize(1000 * 1000, 0);
+            let bytes_read = file.read(&mut buffer.as_mut_slice()).unwrap();
             if bytes_read == 0 {
                 break;
             }
-            log::debug!("Read {} bytes", bytes_read);
-            file_copy.write(&buffer.as_mut_slice())?;
+            if bytes_read < buffer.len() {
+                buffer.resize(bytes_read, 0);
+            }
+            file_copy.write_all(&buffer.as_slice()).unwrap();
+            bytes_written += bytes_read;
         }
-        log::info!("Finished reading and copying file");
+        log::info!("Finished reading and copying file, bytes written: {}", bytes_written);
         //remove the file
 
+        file_copy.flush().unwrap();
         drop(file);
         drop(file_copy);
-        std::fs::remove_file("random_1gb_file.bin")?;
-        if std::fs::metadata("random_1gb_file_copy.bin")?.len() == 0 {
-            std::fs::remove_file("random_1gb_file_copy.bin")?;
+        std::fs::remove_file(file_path).unwrap();
+        if bytes_written == 0 {
+            log::info!("Removing empty file {}", file_path_copy);
+            std::fs::remove_file(&file_path_copy).unwrap();
             break 'outer;
         }
-        std::fs::rename("random_1gb_file_copy.bin", "random_1gb_file.bin")?;
+        std::fs::rename(&file_path_copy, file_path).unwrap();
     }
     Ok(())
 }
 
-
-fn main() -> io::Result<()> {
-    env::set_var("RUST_LOG", env::var("RUST_LOG").unwrap_or("info".to_string()));
+fn main() -> anyhow::Result<()> {
+    env::set_var(
+        "RUST_LOG",
+        env::var("RUST_LOG").unwrap_or("info".to_string()),
+    );
     env_logger::init();
     let args = Args::parse();
-
-
-
-    Ok(())
+    cat_file(&args.file, args.truncate)
 }
---
+++ test.bat
@@ -0,0 +1,2 @@
+copy test_input.zip test_input2.zip
+cargo run -- --file test_input2.zip > test_output.zip
